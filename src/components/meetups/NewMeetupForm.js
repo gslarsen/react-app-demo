a2e7bab7b552f34cd0209065040217234d44641a
Greg Larsen <gslarsen2718@gmail.com>
2022-11-01 12:20:15 -0400

    enable firebase connection to post new meeting json

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,25 +1,55 @@
+import { useRef } from "react";
+
 import Card from "../ui/Card";
 import classes from "./NewMeetupForm.module.css";
 
-function NewMeetupForm() {
+function NewMeetupForm(props) {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription
+    };
+
+    props.onAddMeetup(meetupData);
+  }
+
   return (
     <Card>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <p className={classes.control}>
           <label htmlFor="title">Title</label>
-          <input type="text" id="title" required />
+          <input type="text" id="title" ref={titleInputRef} required />
         </p>
         <p className={classes.control}>
           <label htmlFor="image">Image</label>
-          <input type="url" id="image" required />
+          <input type="url" id="image" ref={imageInputRef} required />
         </p>
         <p className={classes.control}>
           <label htmlFor="address">Address</label>
-          <input type="text" id="address" required />
+          <input type="text" id="address" ref={addressInputRef} required />
         </p>
         <p className={classes.control}>
           <label htmlFor="description">Description</label>
-          <textarea name="description" id="description" rows="5"></textarea>
+          <textarea
+            name="description"
+            id="description"
+            rows="5"
+            ref={descriptionInputRef}
+          ></textarea>
         </p>
         <p className={classes.actions}>
           <button>Add Meetup</button>
