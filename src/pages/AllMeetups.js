64182e892f864bb140a2b694c0f123188850af77
Greg Larsen <gslarsen2718@gmail.com>
2022-11-01 15:15:14 -0400

    set up initial fetch of all meetings from db

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,34 +1,50 @@
+import { useState, useEffect } from "react";
+
 import MeetupList from "../components/meetups/MeetupList";
 
 function AllMeetupsPage() {
-  return (
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://react-getting-started-82346-default-rtdb.firebaseio.com/meetups.json"
+    )
+      .then((response) => {
+        return response.json(); // json() method also returns a promise, so another .then() req'd
+      })
+      .then((data) => {
+        const meetups = [];
+
+        for (const meetupId in data) {
+          const meetup = {
+            id: meetupId,
+            ...data[meetupId]
+          };
+          meetups.push(meetup);
+        }
+
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []); // second parameter to useEffect is array where you list all external values that useEffect relies on, EXCLUDING updating state functions in this component function (e.g. loadedMeetups); in this case, there are none. 
+
+  if (isLoading) {
+    return (
       <section>
-        <h1>All Meetups</h1>
-        <MeetupList meetups={DUMMY_DATA} />
+        <p>Loading...</p>
       </section>
+    );
+  }
+
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <MeetupList meetups={loadedMeetups} />
+    </section>
   );
 }
 
 export default AllMeetupsPage;
 
-/******* DATA **********/
-const DUMMY_DATA = [
-  {
-    id: "m1",
-    title: "This is a first meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-  {
-    id: "m2",
-    title: "This is a second meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a second, more amazing meetup which you definitely should not miss. It will be the best yet!",
-  },
-];
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,10 @@
+import { useHistory } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  const history = useHistory();
+
   function addMeetupHandler(meetupData) {
     fetch("https://react-getting-started-82346-default-rtdb.firebaseio.com/meetups.json", {
       method: "POST",
@@ -8,6 +12,9 @@ function NewMeetupPage() {
       headers: {
         "Content-Type": "application/json"
       }
+    }).then(() => {
+      // history.push() pushes it onto the stack, so back button allows traveling back to previous page
+      history.replace("/");
     });
   }
 
